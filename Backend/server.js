f504127241017e2f94de7902287ed6c95b634013
snehamalbhage <snehamalbhage4@gmail.com>
2024-08-26 13:23:25 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+module.exports = app;
